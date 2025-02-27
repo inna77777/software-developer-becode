5dde9f6d7d1f70bd6c2c59b89347c8e5cf90a0f0
Inna Kobets <innakobetsdev@gmail.com>
2025-02-27 12:13:27 +0100

    completed first part of exo

---
+++ intro.js
@@ -0,0 +1,9 @@
+
+let code = "lili222";
+console.log(code);
+
+let name = "Kobets";
+let firstName = "Inna" ; 
+let city = "Brussels";
+
+console.log(`Your name is ${firstName} ${name} and you live in ${city}.`);
