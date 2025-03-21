33097c5766d105eede015ea22ca8ea8848652143
Inna Kobets <innakobetsdev@gmail.com>
2025-03-21 23:58:32 +0100

    refactor: deleted useless code

--- js-html-css/algo-js/intro.js
+++
@@ -1,9 +0,0 @@
-
-let code = "lili222";
-console.log(code);
-
-let name = "Kobets";
-let firstName = "Inna" ; 
-let city = "Brussels";
-
-console.log(`Your name is ${firstName} ${name} and you live in ${city}.`);
